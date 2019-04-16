28ea423dc6561a6680ef411b38b31273592fdaf3
Jake Lumetta <lumetta@gmail.com>
2019-04-16 10:53:32 -0500

    Adding serialization for Page.PageType

--- page.go
+++ page.go
@@ -1,6 +1,7 @@
 package ButterCMS
 
 type Page struct {
-	Slug 	string                  `json:"slug"`
-	Fields 	map[string]interface{}	`json:"fields"`
+	Slug 		string                  `json:"slug"`
+	PageType 	string                  `json:"page_type"`
+	Fields 		map[string]interface{}	`json:"fields"`
 }
