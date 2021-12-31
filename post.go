9e97da90dfc166697803093fe7e601eea5c90d2b
Jake Lumetta <lumetta@gmail.com>
2021-12-31 11:40:58 -0500

    Adding blog post featured image alt attribute

--- post.go
+++ post.go
@@ -5,17 +5,18 @@ import (
 )
 
 type Post struct {
-	URL             string     `json:"url"`
-	Created         time.Time  `json:"created"`
-	Published       time.Time  `json:"published"`
-	Author          Author     `json:"author"`
-	CategoryList    []Category `json:"categories"`
-	FeaturedImage   string     `json:"featured_image"`
-	Slug            string     `json:"slug"`
-	Title           string     `json:"title"`
-	Body            string     `json:"body"`
-	Summary         string     `json:"summary"`
-	SEOTitle        string     `json:"seo_title"`
-	MetaDescription string     `json:"meta_description"`
-	Status          string     `json:"status"`
+	URL              string     `json:"url"`
+	Created          time.Time  `json:"created"`
+	Published        time.Time  `json:"published"`
+	Author           Author     `json:"author"`
+	CategoryList     []Category `json:"categories"`
+	FeaturedImage    string     `json:"featured_image"`
+	FeaturedImageAlt string     `json:"featured_image_alt"`
+	Slug             string     `json:"slug"`
+	Title            string     `json:"title"`
+	Body             string     `json:"body"`
+	Summary          string     `json:"summary"`
+	SEOTitle         string     `json:"seo_title"`
+	MetaDescription  string     `json:"meta_description"`
+	Status           string     `json:"status"`
 }
